4501c4de182ffd254a1603bfad671abd77272e04
Dmytro Hushchin <dhuchchin@gmail.com>
2023-03-25 14:57:16 +0200

    triangle shape

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,17 +1,18 @@
 import time
 import numpy as np
-import graphics as gr
+from graphics import GraphWin, Polygon, Point
 
-WINDOW_WIDTH = 600
+WINDOW_WEIGHT = 600
 WINDOW_HEIGHT = 600
-CENTER = np.array((WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2))
+CENTER = np.array((WINDOW_WEIGHT // 2, WINDOW_HEIGHT // 2))
 
-ONES = np.ones((4, 1))
+ONES = np.array([
+    [1],
+    [1],
+    [1],
+])
 
 TETA = 3
-T_SPEED = 1.1
-
-M = np.array([[1, 0, 0], [0, 1, 0], [5, 10, 1]])
 
 R = np.array([
     [np.cos(TETA), np.sin(TETA), 0],
@@ -20,70 +21,103 @@ R = np.array([
 ])
 
 T = np.array([
-    [T_SPEED, 0, 0],
-    [0, T_SPEED, 0],
+    [1.05, 0, 0],
+    [0, 1.05, 0],
     [0, 0, 1],
 ])
 
-M1 = np.copy(M)
-M2 = np.copy(M)
+m = np.array([5, 10])
 
-def remove_shape(shape):
-    shape.setOutline("red")
-
-def redraw_shape(points, shape):
-    if shape:
-        remove_shape(shape)
-    shape = gr.Polygon(*[gr.Point(int(x), int(y)) for (x, y) in points])
-    shape.draw(win)
-    return shape
-
-def shape_center(points):
-    return np.array(((points[0][0] + points[2][0]) / 2,
-                     (points[0][1] + points[2][1]) / 2))
-
-def transform(points, transformation):
-    P = np.concatenate((points - shape_center(points), ONES), axis=1)
-    P_stroke = P @ transformation
-    return P_stroke[:, :2] + shape_center(points)
-
-def apply_transformations(diamond_points, transformations, diamond):
-    for i in range(100):
-        for transformation in transformations:
-            diamond_points = transform(diamond_points, transformation)
-        
-        diamond = redraw_shape(diamond_points, diamond)
-
-        if (i // 20) % 2 == 0:
-            transformations[2][0, 0] = T_SPEED
-            transformations[2][1, 1] = T_SPEED
-            transformations[0][2, 0], transformations[0][2, 1] = M[0, 0], M[1, 0]
-            transformations[1][2, 0], transformations[1][2, 1] = M[0, 1], M[1, 1]
-        else:
-            transformations[2][0, 0] = 1 / T_SPEED
-            transformations[2][1, 1] = 1 / T_SPEED
-            transformations[0][2, 0], transformations[0][2, 1] = -M[0, 0], -M[1, 0]
-            transformations[1][2, 0], transformations[1][2, 1] = -M[0, 1], -M[1, 1]
-        
-        time.sleep(0.3)
-        
-    remove_shape(diamond)
-
-diamond_points = np.array([
-    [CENTER[0], CENTER[1] - 50],
-    [CENTER[0] + 25, CENTER[1]],
-    [CENTER[0], CENTER[1] + 50],
-    [CENTER[0] - 25, CENTER[1]],
+M = np.array([
+    [1, 0, 0],
+    [0, 1, 0],
+    [m[0], m[1], 1],
 ])
 
-win = gr.GraphWin("Diamond", WINDOW_WIDTH, WINDOW_HEIGHT)
+T_speed = 1.05
+
+win = GraphWin("Triangle", WINDOW_WEIGHT, WINDOW_HEIGHT)
 win.setBackground('light blue')
 
-diamond1 = None
-diamond2 = None
 
-apply_transformations(diamond_points, [M1, R, T], diamond1)
-apply_transformations(diamond_points, [M2, T, R], diamond2)
+def remove_triangle(triangle):
+    triangle.setFill("light blue")
 
-win.getMouse()
-win.close()
+
+def redraw_triangle(points, triangle):
+    if triangle:
+        remove_triangle(triangle)
+    triangle = Polygon(*[Point(int(x), int(y)) for (x, y) in points])
+    triangle.setFill('orange')
+    triangle.draw(win)
+    return triangle
+
+
+def triangle_center(points):
+    return np.array(((points[0][0] + points[1][0] + points[2][0]) / 3,
+                     (points[0][1] + points[1][1] + points[2][1]) / 3))
+
+
+def transform(points, A):
+    P = np.concatenate(
+        (points - triangle_center(points), ONES),
+        axis=1
+    )
+    P_stroke = P @ A
+    return P_stroke[:, :2] + triangle_center(points)
+
+
+def compositional_transformations(triangle_points):
+    triangle = None
+    for i in range(100):
+        triangle_points = transform(triangle_points, (R @ T @ M))
+        triangle = redraw_triangle(triangle_points, triangle)
+
+        # -----------Change direction-----------
+        if (i // 20) % 2 == 0:
+            T[0, 0], T[1, 1] = T_speed, T_speed
+            M[2, 0], M[2, 1] = m[0], m[1]
+        else:
+            T[0, 0], T[1, 1] = 1 / T_speed, 1 / T_speed
+            M[2, 0], M[2, 1] = -m[0], -m[1]
+        time.sleep(0.3)
+    remove_triangle(triangle)
+
+
+def consecutive_transformations(triangle_points):
+    triangle = None
+    for i in range(100):
+        triangle_points = transform(triangle_points, M)
+        triangle_points = transform(triangle_points, R)
+        triangle_points = transform(triangle_points, T)
+
+        triangle = redraw_triangle(triangle_points, triangle)
+
+        # -----------Change direction-----------
+        if (i // 20) % 2 == 0:
+            T[0, 0], T[1, 1] = T_speed, T_speed
+            M[2, 0], M[2, 1] = m[0], m[1]
+        else:
+            T[0, 0], T[1, 1] = 1 / T_speed, 1 / T_speed
+            M[2, 0], M[2, 1] = -m[0], -m[1]
+        time.sleep(0.3)
+        
+    remove_triangle(triangle)
+    
+
+def main():
+    triangle_points = np.array([
+        [100, 100],
+        [200, 100],
+        [150, 200],
+    ])
+
+    compositional_transformations(triangle_points)
+    consecutive_transformations(triangle_points)
+
+    win.getMouse()
+    win.close()
+    
+    
+if __name__ == '__main__':
+    main()
